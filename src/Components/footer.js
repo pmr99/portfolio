5bba21a2cc677b6117a45d54c13960e2938685e4
pmr99 <pradeep991004@gmail.com>
2022-08-04 23:12:55 +0800

    3rd film

--- src/Components/footer.js
+++ src/Components/footer.js
@@ -1,12 +1,9 @@
 const Footer = () => {
     return(
-        <div>
-            <br/>
         <div style= {{backgroundColor: "#212529"}}>
             <div style = {{ color: "white", padding: "15px"}}> pradeep@berkeley.com
             </div>
 
-        </div>
         </div>
 
     )
--- src/pages/film.js
+++ src/pages/film.js
@@ -12,6 +12,18 @@ import Chance_6 from "../Img/chance_6.png"
 import Chance_7 from "../Img/chance_7.png"
 import Chance_8 from "../Img/chance_8.png"
 import Chance_9 from "../Img/chance_9.png"
+import Potato1 from "../Img/potato1.png"
+import Potato2 from "../Img/potato2.png"
+import Potato3 from "../Img/potato3.png"
+import Potato4 from "../Img/potato4.png"
+import Potato5 from "../Img/potato5.png"
+import Potato6 from "../Img/potato6.png"
+import Potato7 from "../Img/potato7.png"
+import Potato8 from "../Img/potato8.png"
+import Potato9 from "../Img/potato9.png"
+import Potato10 from "../Img/potato10.png"
+
+
 
 import "./film.css"
 import Footer from "../Components/footer"
@@ -30,6 +42,19 @@ const Film = () => {
             <img className = "secondary-pic right" src={Chance_9} alt="Second Chance" width = "1000px"/>
             <img className = "secondary-pic left " src={Chance_6} alt="Second Chance" width = "1000px"/>
             <img className = "secondary-pic right" src={Chance_7} alt="Second Chance" width = "1000px"/>
+            <h2 className = "film-subheader"> "Mr Potato Head" </h2>
+            <h3 className = "film-role"> Director, Writer </h3>   
+            <img className = "secondary-pic left" src={Potato1} alt="Second Chance" width = "1000px"/>
+            <img className = "secondary-pic right" src={Potato2} alt="Second Chance" width = "1000px"/>
+            <img className = "secondary-pic left" src={Potato3} alt="Second Chance" width = "1000px"/>
+            <img className = "secondary-pic right" src={Potato4} alt="Second Chance" width = "1000px"/>
+            <img className = "secondary-pic left" src={Potato5} alt="Second Chance" width = "1000px"/>
+            <img className = "secondary-pic right" src={Potato6} alt="Second Chance" width = "1000px"/>
+            <img className = "secondary-pic left " src={Potato7} alt="Second Chance" width = "1000px"/>
+            <img className = "secondary-pic right" src={Potato8} alt="Second Chance" width = "1000px"/> 
+            <img className = "secondary-pic left " src={Potato9} alt="Second Chance" width = "1000px"/>
+            <img className = "secondary-pic right" src={Potato10} alt="Second Chance" width = "1000px"/> 
+
             <h2 className = "film-subheader"> "Cake By The Ocean" </h2>
             <h3 className = "film-role"> Director, Writer </h3>
             <img className = "main-pic" src={CakePoster} alt="Cake By The Ocean" width = "1000px"/>
@@ -37,6 +62,9 @@ const Film = () => {
             <img className = "secondary-pic right" src={Cake_5} alt="Cake By The Ocean" width = "1000px"/>
             <img className = "secondary-pic left"src={Cake_4} alt="Cake By The Ocean" width = "1000px"/>
             <img className = "secondary-pic right" src={Cake_3} alt="Cake By The Ocean" width = "1000px"/>
+            <br/>
+            <br/>
+            
             <Footer> </Footer>
         </div>
     )
