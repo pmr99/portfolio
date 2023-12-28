09f498349a1a403f5986aca3412f1a0f56dcc9e2
pmr99 <pradeep991004@gmail.com>
2023-12-28 00:30:39 -0800

    fixed alignment issues with safari

--- src/Components/homecard.js
+++ src/Components/homecard.js
@@ -4,14 +4,9 @@ const Homecard = ({link, header, img, description}) => {
     let navigate = useNavigate();
     return(
         <div className = "cardihome" onClick = {() => navigate(link)}>
-            <div className = "home-profile-pic"> {img} </div>
+            <div className = "home-profile-pic" style = {{margin: "auto", marginTop: "0px", marginBottom: "20px" }}> {img} </div>
             <h2 className = "heading"> {header} </h2>
             <p className = "home-profile-text"> {description}</p>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
             
         </div>
 
--- src/pages/home.js
+++ src/pages/home.js
@@ -4,7 +4,6 @@ import {Homecard} from '../Components/homecard';
 import { FaLaptopCode } from "react-icons/fa";
 import { PiFilmSlateBold } from "react-icons/pi";
 import { MdDesignServices } from "react-icons/md";
-import { Container } from 'react-bootstrap';
 
 const intoText = "Hey there, thanks a bunch for dropping by! This space is a mash-up of all the things I'm passionate about. Feel free to dig in and if you wanna chat about anything, just shoot me a message!"
 const designDesription = "I've worked on diverse projects centered around crafting user centric solutions, spanning UI/UX, hardware design, and graphic design alike."
@@ -18,9 +17,9 @@ const Home = () => {
       <h1 className = "title-head"> Pradeep Mani Rathnam </h1>
       <p className = "text" style = {{marginTop: "16px", marginLeft: "25%", marginRight: "25%", textAlign: "center"}}> {intoText} </p>
       <div className = "body-container">
-        <Homecard img = {<MdDesignServices size = "lg"/>} header = "Design" link = "/design" description = {designDesription} />
-        <Homecard img = {<FaLaptopCode size = "lg"/>} header = "Code" link = "/code" description = {codeDescription}/>
-        <Homecard img = {<PiFilmSlateBold size = "lg"/>} header = "Film" link = "/film" description = {filmDescription}/>
+        <Homecard img = {<MdDesignServices size = "120px" style = {{margin : "auto"}}/>} header = "Design" link = "/design" description = {designDesription} />
+        <Homecard img = {<FaLaptopCode size = "120px"/>} header = "Code" link = "/code" description = {codeDescription}/>
+        <Homecard img = {<PiFilmSlateBold size = "120px"/>} header = "Film" link = "/film" description = {filmDescription}/>
       </div>
       </div>
     
